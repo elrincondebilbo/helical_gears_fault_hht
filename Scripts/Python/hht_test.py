03d5b9f4ad0cf2eb6ad0e0ca4eec02c440e8f45d
Mario Tafur <mariop.tafur@gmail.com>
2023-04-17 13:09:02 -0500

    Create hht_test.py

---
+++ Scripts/Python/hht_test.py
@@ -0,0 +1,20 @@
+'''
+----------------------------------------------------------------
+Author: Mario Paul Tafur Díaz
+Institution: Santo Toribio de Mogrovejo Catholic University - Peru
+Contact: mariop.tafur@gmail.com / 71491743@usat.pe
+----------------------------------------------------------------
+
+
+----------------------------------------------------------------
+This file is part of my undergraduate thesis project. The aim
+is to use the Hilbert-Huang Transform to detect early faults in
+helical gears.
+
+This specific file is for testing preliminary data given to me
+by my supervisor.
+
+The results of this code will be compared against the ones 
+obtained with MATLAB.
+----------------------------------------------------------------
+'''
